abd61188d880e569e8669df3a5138df39f3f9bba
Julio M Crespo <jmcrcm89@gmail.com>
2023-11-28 15:10:21 -0800

    entering new code after a clone

---
+++ Models/MenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CateringGuru.Models
+{
+    public class MenuModel
+    {
+        public int ItemName { get; set; }
+        public string ItemPrice { get; set; }
+    }
+}
---
+++ ViewModels/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace CateringGuru.ViewModels
+{
+    internal class ShellViewModel : Screen
+    {
+		private string _itemName;
+
+		public string ItemName
+		{
+			get 
+			{ 
+				return _itemName; 
+			}
+			set 
+			{ 
+				_itemName = value; 
+			}
+		}
+
+	}
+}
